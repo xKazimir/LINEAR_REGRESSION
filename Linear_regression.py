cf2782e4087abb2cfe7c118c55bc7c838d9c3636
Kazimir_ <andrejkazimir.sk@gmail.com>
2025-07-06 23:44:05 +0200

    added a new dataset as well as fitting a line to data

--- Linear_regression.py
+++ Linear_regression.py
@@ -3,9 +3,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-#load data
-df = pd.read_csv("linear_regression_dataset.csv")
+learning_rate = 0.0006
 
+#plotting data with line
 def show_data (data,a,b) :
     #body
     sns.scatterplot(x='x', y='y', data=data)
@@ -19,11 +19,62 @@ def show_data (data,a,b) :
         y_vals.append (a * x_vals[i] + b)
     plt.plot(x_vals, y_vals, color='red', linewidth=2, label=f'y = {a}x + {b}')
     #vykreslenie
-    plt.title('Linear Regression Dataset (Seaborn)')
+    plt.title('Linear Regression Dataset')
     plt.xlabel('x')
     plt.ylabel('y')
     plt.show()
-#SS(mean)
-mean_y = df["y"].mean()
-show_data(df,0,mean_y)
 
+def SSR_der (data,a,b,is_for_intercept) :
+    ssr_der_val = 0
+    for i in range(len(data)) :
+        if is_for_intercept == 0 :
+            ssr_der_val += -2* (int(data["y"][i]) - (a * int(data["x"][i]) + b))
+        else :
+            ssr_der_val += -2* int(data["x"][i])* (int(data["y"][i]) - (a * int(data["x"][i]) + b))
+    return ssr_der_val
+
+def SSR (data,a,b) :
+    ssr_val = 0
+    for i in range(len(data)):
+        ssr_val +=(int(data["y"][i]) - (a * int(data["x"][i]) + b)) ** 2
+    return ssr_val
+
+
+def fit_line_to_data (data) :
+    eps = 1e-6
+    a,b = 1,0
+    ssr_der = 1
+    count = 0
+    eps = 1e-10
+    prev_cost = float('inf')
+
+    while count < 10000:
+        b -= SSR_der(data,a,b,0) * learning_rate
+        a -= SSR_der(data, a, b, 1) * learning_rate
+        print ("Iteration :" + str(count) + " A:"+ str(a) + " B:"+ str(b) )
+        if count % 500 == 0 :
+            show_data (data,a,b)
+        count += 1
+        ssr = SSR(data, a, b)
+        if abs(prev_cost - ssr) < eps :
+            break
+        prev_cost = ssr
+
+
+def main():
+    # load data
+    df = pd.read_csv("linear_regression_dataset.csv")
+    #df = pd.read_csv("linear_regression_dataset_2.csv")
+
+    # SS(mean)
+    mean_y = df["y"].mean()
+    show_data(df, 0, mean_y)
+    fit_line_to_data(df)
+
+    x = df["x"].values
+    y = df["y"].values
+
+    # deg=1 znamená lineárny tvar y = a*x + b
+    a, b = np.polyfit(x, y, deg=1)
+    print(f"NumPy polyfit: y = {a:.4f}x + {b:.4f}")
+main ()
